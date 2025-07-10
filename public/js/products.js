555eef1e2d3b8e1805c4bbd3114ece8cc4f70919
Ndegwa Jackson <128777440+jackdevs-lab@users.noreply.github.com>
2025-07-10 16:31:35 +0300

    updates

--- public/js/products.js
+++ public/js/products.js
@@ -1,4 +1,4 @@
-const API_BASE_URL ="https://glossier-backend.onrender.com";
+const API_BASE_URL ="https://railway.com/project/62059eac-5ebc-4d58-a8bc-5e34c32e2c1d";
 
 async function getProductsByCategory(category) {
     try {
